7d1c2801133110f8b0caa99580c0d1d894ebb905
Hakan ERGUL <hknrgl@gmail.com>
2019-07-29 18:59:23 +0300

    Update FCC23.js

--- FCC23.js
+++ FCC23.js
@@ -3,36 +3,32 @@ function checkCashRegister(price, cash, cid) {
   let cashUnit = {'PENNY':0.01, 'NICKEL': 0.05, 'DIME': 0.1, 'QUARTER': 0.5, 'ONE': 1, 'FIVE': 5, 'TEN': 10, 'TWENTY': 20, 'ONE HUNDRED': 100 };
   let balance = cash-price;
   let paraUstu = 0;
-  let degisim = cid;
+  let degisim = JSON.parse(JSON.stringify(cid));
 	for(let i=degisim.length-1; i >= 0; i--){
 		//console.debug("---",cid[i][1], cashUnit[cid[i][0]]);
   		if(degisim[i][1] > 0 && (degisim[i][1] >= cashUnit[degisim[i][0]])){
   			
   			if(cashUnit[degisim[i][0]] <= balance){
-  				while(balance-cashUnit[degisim[i][0]]>=0 && cid[i][1]>0){
+  				while(balance-cashUnit[degisim[i][0]]>=0 && degisim[i][1]>0){
   					balance-= cashUnit[degisim[i][0]];
   					degisim[i][1]-=cashUnit[degisim[i][0]];
-  					balance = balance.toFixed(3);
+  					balance = balance.toFixed(2);
   					console.log('=='+balance + '---'+ cashUnit[degisim[i][0]]+' var olan : '+ degisim[i]);
   				}
 
-  				if(degisim[i][1] === cid[i][1]){
-  					console.log(cid[i][1])
-  					change.change.push([cid[i][0], degisim[i][1]]);
-  					console.log('sokkk');
+  				if(degisim[i][1] !== cid[i][1]){
+  					change.change.push([cid[i][0], (cid[i][1]-degisim[i][1]).toFixed(2)]);
   				}
   			}
   		}
   }
 
-  console.debug(balance, cid);
+  console.debug(change);
  /* if (balance<0){
-
   		change.status='INSUFFICIENT_FUNDS';
   		change.change=[];
  	} else if (balance>0){
 	  	change.status='OPEN';
-
 	} else {
  		change.status='CLOSED';
   }
@@ -52,4 +48,4 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60],
 // ["ONE HUNDRED", 100]]
 
-console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
